01be3f79a1eeaf8b1cff4999d767c8ce25da317f
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2022-11-23 14:11:01 +1300

    Worked on Menu & Added Code pasting

--- Assets/Blues Stuff/Scripts/MenuManager.cs
+++ Assets/Blues Stuff/Scripts/MenuManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
@@ -13,6 +15,8 @@ public class MenuManager : MonoBehaviour
     public float ButtonHoverTime;
     public float ButtonPressScale;
 
+    public GameObject[] disableOnStart;
+
     private void Awake()
     {
         if (Singleton == null)
@@ -23,5 +27,38 @@ public class MenuManager : MonoBehaviour
         {
             Debug.LogError("BNlue you ideiot");
         }
+
+        foreach (GameObject go in disableOnStart)
+        {
+            go.SetActive(false);
+        }
+    }
+    private void DeleteCaret()
+    {
+        foreach (TMP_SelectionCaret item in GetComponentsInChildren<TMP_SelectionCaret>())
+        {
+            item.transform.localScale = new Vector3(item.transform.localScale.x, item.transform.localScale.y * 0.7f, item.transform.localScale.z);
+        }
+    }
+    public void InitPrivateLobbyMenu()
+    {
+        Invoke("DeleteCaret", 0.5f);
+    }
+
+    public void Exit() 
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+    
+    public void PasteFromClipBoard(TMP_InputField text)
+    {
+        TextEditor textEditor = new();
+        textEditor.multiline = true;
+        textEditor.Paste();
+
+        Debug.Log(textEditor.text);
+
+        text.text = textEditor.text;
     }
 }
