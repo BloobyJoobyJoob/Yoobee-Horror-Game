79c8bf32a113f5b41f401b9a31be6407d0c8df63
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2022-12-06 18:34:20 +1300

    Worked on fpv controller

--- Assets/Blues Stuff/Scripts/PlayerController.cs
+++ Assets/Blues Stuff/Scripts/PlayerController.cs
@@ -1,48 +1,107 @@
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : NetworkBehaviour
 {
+    #region Vars
+
+    public static PlayerController ClientManager = null;
+    public static PlayerController TeammateManager = null;
+
+    public CinemachineVirtualCamera Camera;
+
     public float SprintSpeed;
     public float SneakSpeed;
     public float WalkSpeed;
 
-    private CharacterController controller;
-    private PlayerInput input;
+    public float BackwardsSpeedMultiplier = 0.5f;
+    public float SidewaysSpeedMultiplier = 0.8f;
 
-    Vector2 walk;
+    private CharacterController controller;
+
     float Speed;
 
-    Vector2 movement;
+    Vector2 inputMovement;
+    Vector3 gravityMovement;
+    Vector3 walkingMovement;
     bool sprint;
     bool sneak;
+
+    CinemachinePOV cameraPOV;
+
+    #endregion
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
-        input = GetComponent<PlayerInput>();
+        cameraPOV = Camera.GetCinemachineComponent<CinemachinePOV>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            if (ClientManager == null)
+            {
+                ClientManager = this;
+            }
+            else
+            {
+                Debug.LogError("Blue you smelly fart");
+                return;
+            }
+        }
+        else
+        {
+            if (TeammateManager == null)
+            {
+                TeammateManager = this;
+            }
+            else
+            {
+                Debug.LogError("Blue you smelly fart");
+                return;
+            }
+        }
     }
 
     private void Update()
     {
+        Move();
+    }
+    void Move()
+    {
+        Vector2 walkSpeed;
+
         Speed = sneak ? SneakSpeed : sprint ? SprintSpeed : WalkSpeed;
 
-        if (movement.y > 0)
-        {
-            walk.y = 
-        }
-        else if (movement.y < 0)
-        {
+        walkSpeed.y = inputMovement.y < 0 ? inputMovement.y * Speed * BackwardsSpeedMultiplier : Speed * inputMovement.y;
+        walkSpeed.x = inputMovement.x * Speed;
 
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + cameraPOV.m_HorizontalAxis.Value, transform.rotation.z);
+
+        walkingMovement = (transform.forward * walkSpeed.y) + (transform.right * walkSpeed.x);
+
+        if (controller.isGrounded)
+        {
+            gravityMovement = Vector3.zero;
+        }
+        else
+        {
+            gravityMovement += Physics.gravity * Time.deltaTime;
         }
 
-        controller.Move(Speed * Time.deltaTime * transform.forward);
+        controller.Move((walkingMovement + gravityMovement) * Time.deltaTime);
     }
 
+    #region GetInput
     public void OnWalk(InputAction.CallbackContext context)
     {
-        movement = context.ReadValue<Vector2>();
+        inputMovement = context.ReadValue<Vector2>();
     }
     public void OnSprint(InputAction.CallbackContext context)
     {
@@ -52,4 +111,5 @@ public class PlayerController : NetworkBehaviour
     {
         sneak = context.action.IsPressed();
     }
+    #endregion
 }
